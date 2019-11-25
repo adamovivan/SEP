893318514d3be4961c2170dcf800e3b52760d673
adamovivan <adamovivan@gmail.com>
2019-11-25 15:29:27 +0100

    Payment concetrator init

---
+++ payment_concetrator/src/test/java/rs/ac/uns/ftn/payment_concentrator/PaymentConcentratorApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.payment_concentrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentConcentratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
