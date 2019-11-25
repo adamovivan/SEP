b8c46f406a7a9b2955e1c02df05638db60033258
Dejan Predojevic <dejanpredojevic11@gmail.com>
2019-11-25 14:35:09 +0100

    fix

--- PaypalAPI/src/main/java/com/example/demo/PaypalController.java
+++ PaypalAPI/src/main/java/com/example/demo/PaypalController.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
 import com.paypal.base.rest.PayPalRESTException;
 
+@RestController
 public class PaypalController {
 
 	@Autowired
@@ -21,26 +25,23 @@ public class PaypalController {
 	 * @return
 	 */
 	@PostMapping("/createPayment")
-	public Map<String, Object> payment(@ModelAttribute("order") Order order) {
-		    Map<String,Object> paymentCredencial = null;
-			try {
-				/*
-				 * Dva moguca slucaja po povratku sa peypala na osnovu dva url koja su prosledjena:
-				 * 1. Ako sve se desi problem -> CANCEL se hendluje na frontu ponistavanjem svega vracanjem na korpu ponovnim pokusajem ...
-				 * 2. Ako sve prodje okej -> SUCCESS se hendluje na frontu tako sto kada korisnik potvrdi sve , peypal nazad vraca credencijale,
-				 *  odnosno String paymentID,String payerID , i token-koji nam nije bitan ... Sa ovim credencijalima potrebno
-				 *  je sa fronta po uspesnoj autentifikacijisa peypalom i potvrdi naloga pozvati metodu completePayment da bi se
-				 *  izvrsilo placanje...
-				 */
-				paymentCredencial = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
-						order.getIntent(), order.getDescription(), "http://localhost:4200/cancel",
-						"http://localhost:4200/success");
-				
-			} catch (PayPalRESTException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	public String payment(@RequestBody Order order) {
+		System.out.println("Bla: " + order.getIntent() + order.getPrice());
+		try {
+			Payment payment = service.createPayment(order.getPrice(),order.getIntent(),
+					"http://localhost:4200/cancel",
+					"http://localhost:4200/success");
+			for(Links link:payment.getLinks()) {
+				if(link.getRel().equals("approval_url")) {
+					return link.getHref();
+				}
 			}
-			return paymentCredencial;
+			
+		} catch (PayPalRESTException e) {
+		
+			e.printStackTrace();
+		}
+		return "";
 	}
 	
 	/**
@@ -49,7 +50,7 @@ public class PaypalController {
 	 * @return
 	 */
 	@PostMapping(value = "/completePayment")
-	public Map<String, Object> completePayment(@ModelAttribute("credencial") Credencial credencial){
+	public Map<String, Object> completePayment(@RequestBody Credencial credencial){
 	    return service.completePayment(credencial.getPaymentID(),credencial.getPayerID());
 	}
 	
--- PaypalAPI/src/main/java/com/example/demo/PaypalService.java
+++ PaypalAPI/src/main/java/com/example/demo/PaypalService.java
@@ -1,21 +1,22 @@
 package com.example.demo;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.paypal.api.payments.Amount;
-import com.paypal.api.payments.Links;
 import com.paypal.api.payments.Payer;
 import com.paypal.api.payments.Payment;
 import com.paypal.api.payments.PaymentExecution;
 import com.paypal.api.payments.RedirectUrls;
 import com.paypal.api.payments.Transaction;
 import com.paypal.base.rest.APIContext;
-import com.paypal.base.rest.OAuthTokenCredential;
 import com.paypal.base.rest.PayPalRESTException;
 
 
@@ -27,82 +28,45 @@ import com.paypal.base.rest.PayPalRESTException;
 @Service
 public class PaypalService {
 	
-	String clientId = "AWure9h6GXJwJ3YEfVd5YPIpnqrldESUsElWx9B5Kr8Ykc4_cvz9QM5KaTv4Jh743W3B_PMEBtjFfuJd";
-	String clientSecret = "EBOgOZbYvvThwaeKGiPgTTjYs4NIxGuNZEcE6SPnshx_K-GgspvKhgQzBfbguYJ2ioq1kGMVfu6k7bc0";
+	@Autowired
+	private APIContext context;
 	
 	/*
 	 * 1. Payment Creation;
 	 * 2. Payment Confirmation;
 	 */
-	public Map<String, Object> createPayment(
-			Double total, 
-			String currency, 
-			String method,
+	public Payment createPayment(
+			Double total,
 			String intent,
-			String description, 
 			String cancelUrl, 
 			String successUrl) throws PayPalRESTException{
-		
-		Map<String, Object> response = null;
-		//postavljanje ukupne cene za placanje
 		Amount amount = new Amount();
-		amount.setCurrency(currency);
-		amount.setTotal(Double.toString(total));
+		amount.setCurrency("USD");
+		total = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP).doubleValue();
+		amount.setTotal(String.format("%.2f", total));
 
-		//opis transakcije i suma
 		Transaction transaction = new Transaction();
-		transaction.setDescription(description);
 		transaction.setAmount(amount);
 
-		
 		List<Transaction> transactions = new ArrayList<>();
 		transactions.add(transaction);
 
-		//postavljanje nacina placanja
 		Payer payer = new Payer();
-		payer.setPaymentMethod(method.toString());
+		payer.setPaymentMethod("paypal");
 
-		//Kreiranje placanja
 		Payment payment = new Payment();
-		payment.setIntent(intent.toString());
+		payment.setIntent(intent);
 		payment.setPayer(payer);  
 		payment.setTransactions(transactions);
-		
-		
 		RedirectUrls redirectUrls = new RedirectUrls();
 		redirectUrls.setCancelUrl(cancelUrl);
 		redirectUrls.setReturnUrl(successUrl);
 		payment.setRedirectUrls(redirectUrls);
-		Payment createdPayment;
-		try {
-	        String redirectUrl = "";
-	        Map<String, String> configMap = new HashMap<>();
-			configMap.put("mode", "sandbox");
-	        OAuthTokenCredential oAuthTokenCredential = new OAuthTokenCredential(clientId, 
-	        																	clientSecret, configMap);
-	        
-	        APIContext context = new APIContext(oAuthTokenCredential.getAccessToken());
-	        context.setConfigurationMap(configMap);
-	        
-	        createdPayment = payment.create(context);
-	        if(createdPayment!=null){
-	            List<Links> links = createdPayment.getLinks();
-	            for (Links link:links) {
-	                if(link.getRel().equals("approval_url")){
-	                    redirectUrl = link.getHref();
-	                    break;
-	                }
-	            }
-	            response = new HashMap<String, Object>();
-	            response.put("status", "success");
-	            response.put("redirect_url", redirectUrl);
-	        }
-	    } catch (PayPalRESTException e) {
-	        System.out.println("Error happened during payment creation!");
-	    }
-	    return response;
-	    
+
+		return payment.create(context);
 	}
+	
+	
 	
 	public Map<String, Object> completePayment(String paymentID,String payerID){
 	    Map<String, Object> response = new HashMap<String, Object>();
@@ -112,12 +76,6 @@ public class PaypalService {
 	    PaymentExecution paymentExecution = new PaymentExecution();
 	    paymentExecution.setPayerId(payerID);
 	    try {
-	    	 Map<String, String> configMap = new HashMap<>();
-			configMap.put("mode", "sandbox");
-	        OAuthTokenCredential oAuthTokenCredential = new OAuthTokenCredential(clientId, 
-	        																	clientSecret, configMap);
-	        
-	        APIContext context = new APIContext(oAuthTokenCredential.getAccessToken());
 	        Payment createdPayment = payment.execute(context, paymentExecution);
 	        if(createdPayment!=null){
 	            response.put("status", "success");
