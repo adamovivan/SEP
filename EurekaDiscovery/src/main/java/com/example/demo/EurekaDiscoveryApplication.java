71ce18b9cc380c3f9f0c69ad9de57f27fbc5d9d3
Dejan Predojevic <dejanpredojevic11@gmail.com>
2019-11-25 19:07:04 +0100

    zull

--- EurekaDiscovery/src/main/java/com/example/demo/EurekaDiscoveryApplication.java
+++ EurekaDiscovery/src/main/java/com/example/demo/EurekaDiscoveryApplication.java
@@ -2,14 +2,15 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
-@EnableEurekaClient
-public class PaypalApiApplication {
+@EnableEurekaServer
+public class EurekaDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PaypalApiApplication.class, args);
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
 	}
 
 }
