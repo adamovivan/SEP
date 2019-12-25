558615f0976db23a73c6e5153f93d13f9f620327
Dejan Predojevic <dejanpredojevic11@gmail.com>
2019-12-25 19:57:16 +0100

    nc skinut https

--- scientific_center/src/main/java/rs/ac/uns/ftn/scientific_center/config/Config.java
+++ scientific_center/src/main/java/rs/ac/uns/ftn/scientific_center/config/Config.java
@@ -1,28 +1,29 @@
 package rs.ac.uns.ftn.scientific_center.config;
 
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.impl.client.HttpClients;
-import org.springframework.beans.factory.annotation.Value;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+//import org.apache.http.impl.client.HttpClients;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import org.apache.http.ssl.SSLContextBuilder;
 
-import javax.net.ssl.SSLContext;
+//import org.apache.http.ssl.SSLContextBuilder;
 
 @Configuration
 public class Config {
 
-    @Value("${http.client.ssl.trust-store}")
+   /* @Value("${http.client.ssl.trust-store}")
     private String truststorePath;
 
     @Value("${http.client.ssl.trust-store-password}")
     private String truststorePassword;
+*/
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
 
 //    @Value("${http.client.ssl.key-store}")
 //    private String keystorePath;
@@ -30,7 +31,7 @@ public class Config {
 //    @Value("${http.client.ssl.key-store-password}")
 //    private String keystorePassword;
 
-    @Bean
+    /*@Bean
     public RestTemplate restTemplate() throws Exception {
 
         Resource trustStore = new ClassPathResource(truststorePath);
@@ -44,5 +45,5 @@ public class Config {
                 .build();
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return new RestTemplate(factory);
-    }
+    }*/
 }
